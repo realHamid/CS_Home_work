f05e8f8183b8f87aa28b246213941fff1a75e9b1
Hamid Rahimi <1122hamidjan@gmail.com>
2020-10-15 22:30:30 +0430

    search working

--- view/list_real_estate.php
+++ view/list_real_estate.php
@@ -1,4 +1,49 @@
-<?php require_once( '_config.php' )?>
+<?php
+
+    require_once( '_config.php' );
+
+    if(isset($_POST['search'])){
+
+        $condition = '';
+
+        if(!empty($_POST['province_id'])){
+            $province_id = VD($_POST['province_id']);
+            $condition = " AND province_id = '$province_id'  ";
+        }
+
+        if(!empty($_POST['district_id'])){
+            $district_id = VD($_POST['district_id']);
+            $condition = " AND district_id = '$district_id'  ";
+        }
+
+        if(!empty($_POST['category_id'])){
+            $category_id = VD($_POST['category_id']);
+            $condition = " AND category_id = '$category_id'  ";
+        }
+
+        if(!empty($_POST['type'])){
+            $type = VD($_POST['type']);
+            $condition = " AND type = '$type'  ";
+        }
+
+        if(!empty($_POST['owner_name'])){
+            $owner_name = VD($_POST['owner_name']);
+            $condition = " AND owner_name like '%$owner_name%'  ";
+        }
+
+        if(!empty($_POST['owner_email'])){
+            $owner_email = VD($_POST['owner_email']);
+            $condition = " AND owner_email = '$owner_email'  ";
+        }
+
+        $sql = $db->query("SELECT * FROM real_estates where id != 'null' $condition order by id desc  ");
+
+    }else {
+        $sql = $db->query("SELECT * FROM real_estates order by id desc  ");
+    }
+
+
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,6 +82,122 @@
                 </div>
             </div> <!-- /. Content Header (Page header) -->
 
+            <div class="row">
+
+                <div class="col-sm-12 col-md-12">
+                    <div class="panel panel-info lobidisable">
+
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                <h5 class="bfont"  >  برای جستجو مشخصات را وارد کنید.   </h5>
+                            </div>
+                        </div>
+
+                        <div class="panel-body">
+                            <br />
+
+                            <form id="validation-form" class="form-horizontal form-label-left" method="post" enctype="multipart/form-data" action="#">
+
+                                <input type="hidden" name="search" value="1">
+
+                                <div class="item form-group " >
+                                    <label class="control-label col-md-3 col-sm-12 col-xs-12 col-lg-3 bfont" for="province_id">  ولایت     <span class="required">*</span></label>
+                                    <div class="col-md-5 col-sm-12 col-xs-12 col-lg-5">
+                                        <select class="basic-single form-control"  dir="rtl" name="province_id" id="province_id" >
+                                            <option  value=""> یکی راه انتخاب کنید  </option>
+                                            <?php
+
+                                            $view_slider = $db->query("SELECT * FROM provinces ORDER BY id DESC ");
+                                            foreach ( $view_slider as $row) {
+                                                if(isset($_POST['province_id']) and $_POST['province_id'] == $row['id'])
+                                                    echo  ' <option selected value="'.$row['id'].'" >  '.$row['name'].'  </option> ';
+                                                else
+                                                    echo  ' <option value="'.$row['id'].'" >  '.$row['name'].'  </option> ';
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="item form-group " >
+                                    <label class="control-label col-md-3 col-sm-12 col-xs-12 col-lg-3 bfont" for="district_id">  ولسوالی      <span class="required">*</span></label>
+                                    <div class="col-md-5 col-sm-12 col-xs-12 col-lg-5">
+                                        <select class="basic-single form-control"  dir="rtl" name="district_id" id="district_id" >
+                                            <option value=""> یکی راه انتخاب کنید  </option>
+                                            <?php
+
+                                            $view_slider = $db->query("SELECT * FROM districts ORDER BY id DESC ");
+                                            foreach ( $view_slider as $row) {
+                                                if(isset($_POST['district_id']) and $_POST['district_id'] == $row['id'])
+                                                    echo  ' <option selected value="'.$row['id'].'" >  '.$row['name'].'  </option> ';
+                                                else
+                                                    echo  ' <option value="'.$row['id'].'" >  '.$row['name'].'  </option> ';
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+
+                                <div class="item form-group " >
+                                    <label class="control-label col-md-3 col-sm-12 col-xs-12 col-lg-3 bfont" for="category_id">  دسته بندی      <span class="required">*</span></label>
+                                    <div class="col-md-5 col-sm-12 col-xs-12 col-lg-5">
+                                        <select class="basic-single form-control"  dir="rtl" name="category_id" id="category_id" >
+                                            <option value=""> یکی راه انتخاب کنید  </option>
+                                            <?php
+
+                                            $view_slider = $db->query("SELECT * FROM categories ORDER BY id DESC ");
+                                            foreach ( $view_slider as $row) {
+                                                if(isset($_POST['category_id']) and $_POST['category_id'] == $row['id'])
+                                                    echo  ' <option selected value="'.$row['id'].'" >  '.$row['name'].'  </option> ';
+                                                else
+                                                    echo  ' <option value="'.$row['id'].'" >  '.$row['name'].'  </option> ';
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="item form-group " >
+                                    <label class="control-label col-md-3 col-sm-12 col-xs-12 col-lg-3 bfont" for="type">  نوعیت       <span class="required">*</span></label>
+                                    <div class="col-md-5 col-sm-12 col-xs-12 col-lg-5">
+                                        <select class="basic-single form-control"  dir="rtl" name="type" id="type" >
+                                            <option value=""> یکی راه انتخاب کنید  </option>
+                                            <option <?php if(isset($_POST['type']) and $_POST['type'] == "payment" ) echo "selected" ?> value="payment"> فروشی  </option>
+                                            <option <?php if(isset($_POST['type']) and $_POST['type'] == "rent" ) echo "selected" ?> value="rent"> کرای </option>
+                                            <option <?php if(isset($_POST['type']) and $_POST['type'] == "hostage" ) echo "selected" ?> value="hostage">گروی </option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="item form-group">
+                                    <label class="control-label col-md-3 col-sm-12 col-xs-12 col-lg-3 bfont" for="owner_name">  نام صاحب   <span class="required">*</span></label>
+                                    <div class="col-md-5 col-sm-12 col-xs-12 col-lg-5">
+                                        <input type="text" id="owner_name" value="<?php if(isset($_POST['owner_name'])) echo $_POST['owner_name']?>" name="owner_name" class="form-control cfont" placeholder="">
+                                    </div>
+                                </div>
+
+                                <div class="item form-group">
+                                    <label class="control-label col-md-3 col-sm-12 col-xs-12 col-lg-3 bfont" for="owner_email">  ایمیل صاحب      </label>
+                                    <div class="col-md-5 col-sm-12 col-xs-12 col-lg-5">
+                                        <input type="text" id="owner_email" value="<?php if(isset($_POST['owner_email'])) echo $_POST['owner_email'] ?>" name="owner_email" class="form-control cfont" placeholder="">
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="col-lg-9 col-lg-offset-3">
+                                        <button type="submit" class="btn btn-inverse w-md m-b-5 bfont" name = "submit"> &nbsp;<i class="fa fa-search"> </i>&nbsp;&nbsp;  جستجو   &nbsp;</button>
+                                        <a href="list_real_estate.php" type="reset" class="btn btn-danger w-md m-b-5 bfont"> &nbsp; <i class="fa fa-refresh"> </i>&nbsp;&nbsp;  تازه سازی  </a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
             <!-- start table  -->
             <div class="row">
                 <div class="col-sm-12">
@@ -65,8 +226,8 @@
                                     <tbody>
                                     <?php
                                     $count = 1;
-                                    $sql = $db->query("SELECT * FROM real_estates order by id desc  ");
                                     $status = ['payment' => 'فروشی' , 'rent' => 'کرای' , 'hostage' => 'گروی'];
+
                                     if($sql->rowCount()>0){
                                         while ($row = $sql->fetch()) {
 
@@ -93,7 +254,7 @@
                                     }else{
                                         echo "
                                             <tr>
-                                                <td colspan='12' class='alert alert-danger' > کدام ولایت  به سیستم اضافه نشده است.  </td>
+                                                <td colspan='12' class='alert alert-danger' > کدام ملکی  به سیستم اضافه نشده است.  </td>
                                             </tr>
                                         ";
                                     }
